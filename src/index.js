364a25269acec0d69cbd4b7076062cd0eb5fd399
abdullah-rao1 <abdullahrao5515@gmail.com>
2026-08-10 15:20:29 +0500

    node js issue removal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+if (!globalThis.crypto) {
+    globalThis.crypto = require('crypto').webcrypto;
+}
+require('dotenv').config();
+
 require('dotenv').config();
 const express = require('express');
 const whatsapp = require('./whatsapp');
